a972c7f95af2a4fd7e487a55ab0b6f0f824d4e91
TheMechanist1 <44752216+TheMechanist1@users.noreply.github.com>
2019-03-13 11:50:10 -0500

    Added a button and stuff

--- 3-13-19.py
+++ 3-13-19.py
@@ -1,1 +1,30 @@
-from Tkinter
+from Tkinter import *
+import tkFileDialog
+import PIL
+import PIL.ImageDraw
+import PIL.ImageTk
+import os
+
+
+
+root = Tk()
+root.geometry("500x500")
+# root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+frame = Frame(root)
+
+canvas = Canvas(root, width = 300, height = 300)      
+canvas.pack() 
+
+def file():
+    root.filename = tkFileDialog.askopenfilename()
+    img = PIL.Image.open(root.filename)
+    pi = ImageTk.PhotoImage(img)
+    canvas.create_image((1,1),image=pi)
+    # canvas.create_image(20,20,image=img)
+    # PIL.ImageDraw.Draw(img)
+
+b = Button(root, text="OK", command=file)
+b.pack()
+
+     
+root.mainloop()
