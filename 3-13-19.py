eb90de7e28e1617019311b76310bcf05feeffd49
Mechanist <dminer625@gmail.com>
2019-03-17 18:16:32 -0500

    STUFFS WORKING!!!!!

--- 3-13-19.py
+++ 3-13-19.py
@@ -5,28 +5,32 @@ import PIL.ImageDraw
 import PIL.ImageTk
 import os
 
-
-
 root = Tk()
 root.geometry("1000x1000")
 frame = Frame(root)
 imgList = []
+pImgList = []
 
-canvas = Canvas(root, width = 500, height = 500)
+canvas = Canvas(root, width = 1000, height = 500)
 canvas.pack() 
 
 def file():
-    files = tkFileDialog.askopenfiles()
-    for i in files:
-        imgList.append(i)
-        
     
-    img = PIL.Image.open(root.filename).resize((100, 100))
+    directory = tkFileDialog.askdirectory()
+    files = os.listdir(directory)
+    neatPath = [os.path.join(directory, i) for i in files]
     
-    pi = PIL.ImageTk.PhotoImage(img)
-    
-    canvas.create_image((img.width/2 ,img.height/2),image=pi)
-    PIL.ImageDraw.Draw(pi)
+    for paths in neatPath:
+        try:
+            imgList.append(PIL.Image.open(paths).resize((100, 100)))
+        except IOError:
+            continue
+
+    for index, img in enumerate(imgList):
+        pi = PIL.ImageTk.PhotoImage(img)
+        pImgList.append(pi)
+        canvas.create_image((img.width/2 + index*100, img.height/2),image=pi)
+    canvas.update()
 
 b = Button(root, text="Chose File", command=file)
 b.pack()
