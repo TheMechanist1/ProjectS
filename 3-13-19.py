5872b1e0486252768058cb8e25934c81e9f6dd19
TheMechanist1 <44752216+TheMechanist1@users.noreply.github.com>
2019-03-18 10:40:42 -0500

    Version 5
    added comments and canvas stuff

--- 3-13-19.py
+++ 3-13-19.py
@@ -8,32 +8,59 @@ import os
 root = Tk()
 root.geometry("1000x1000")
 frame = Frame(root)
+buttonList = []
 imgList = []
 pImgList = []
 
-canvas = Canvas(root, width = 1000, height = 500)
-canvas.pack() 
+
+
+
 
 def file():
+    #ask for the directory
+    #Todo: remove the path before you deploy
+    directory = tkFileDialog.askdirectory(initialdir="K:\\EngineerTech\\AP Comp Science Principals\\14KeithA\\22CooksleyD\\pics")
     
-    directory = tkFileDialog.askdirectory()
+    #list all files in the directory
     files = os.listdir(directory)
+    
+    #Get the full path of the images. ex "img1.png" to "c:/users/22cooksleyd/pictures/img1.png"
     neatPath = [os.path.join(directory, i) for i in files]
     
+    #go through all the files in the directory
     for paths in neatPath:
         try:
             imgList.append(PIL.Image.open(paths).resize((100, 100)))
+            
+            #ignore the file if its not an image
         except IOError:
             continue
 
+    #add all the images in imgList into the canvas and then update the canvas so they show up
     for index, img in enumerate(imgList):
         pi = PIL.ImageTk.PhotoImage(img)
         pImgList.append(pi)
         canvas.create_image((img.width/2 + index*100, img.height/2),image=pi)
     canvas.update()
 
-b = Button(root, text="Chose File", command=file)
-b.pack()
 
-     
+
+
+#make the canvas and button
+canvas = Canvas(root, width = 1000, height = 1000)
+buttonList.append(Button(root, text="Chose File", command=file, x=0, y=0))
+
+#Create a rectangle for the buttons to go in
+canvas.create_rectangle(0, 0, 75, 700, fill="#476042")
+
+
+#Init the button before the canvas so it shows up above the canvas
+for button in buttonList:
+    button.pack()
+    
+canvas.pack() 
+
+
+
+#start the program
 root.mainloop()
