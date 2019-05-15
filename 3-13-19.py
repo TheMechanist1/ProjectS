da1e49fccfc2af2dcf59a2be5522a1bf4340a43f
TheMechanist1 <44752216+TheMechanist1@users.noreply.github.com>
2019-05-15 10:27:17 -0500

    ggood
    ghhh

--- 3-13-19.py
+++ 3-13-19.py
@@ -7,6 +7,7 @@ import PIL.ImageFont
 
 import PIL.ImageTk
 import os
+import numpy
 
 root = Tk()
 root.geometry("1000x1000")
@@ -17,7 +18,21 @@ pImgList = []
 
 
 
+def render(list):
+    y = 0
+    for index, img in enumerate(list):
+        draw = PIL.ImageDraw.Draw(img)
+        font = PIL.ImageFont.truetype('K:\\EngineerTech\\AP Comp Science Principals\\14KeithA\\22Cooksleyd\\ProjectS\\Roboto-Bold.ttf', size=10)
+        color = 'RGBA(255, 255, 255, 255)'
+        draw.text((25, 25), "Cooksley Co.", fill=color, font=font)
+        pi = PIL.ImageTk.PhotoImage(img)
+        pImgList.append(pi)
+        if(img.width*index > 1000):
+            y += 100
+            
+        canvas.create_image((img.width/2 + index*img.width, img.height/2 + y),image=pi)
 
+    canvas.update()
 
 def file():
     #ask for the directory
@@ -42,22 +57,25 @@ def file():
             continue
 
     #add all the images in imgList into the canvas and then update the canvas so they show up
-    for index, img in enumerate(imgList):
+    render(imgList)
 
-        draw = PIL.ImageDraw.Draw(img)
-        font = PIL.ImageFont.truetype('K:\\EngineerTech\\AP Comp Science Principals\\14KeithA\\22Cooksleyd\\ProjectS\\Roboto-Bold.ttf', size=10)
-        color = 'RGBA(255, 0, 0, 100)'
-        draw.text((0, 0), "Cooksley Co.", fill=color, font=font)
-        pi = PIL.ImageTk.PhotoImage(img)
-        pImgList.append(pi)
-        canvas.create_image((img.width/2 + index*img.width, img.height/2),image=pi)
+def negate():
+    i = 0
+    for img in imgList:
+        colors = numpy.asarray(img)
+        
+        for col in colors:
+            for color in col:
+                color[0] *= .5
 
-    canvas.update()
-
+        imgs = PIL.Image.fromarray(numpy.uint8(colors))
+        imgList[i] = imgs
+        i = i + 1
+    render(imgList)
 #make the canvas and button
 canvas = Canvas(root, width = 1000, height = 1000)
 buttonList.append(Button(root, text="Chose File", command=file))
-
+buttonList.append(Button(root, text="Negative", command=negate))
 
 
 #Init the button before the canvas so it shows up above the canvas
